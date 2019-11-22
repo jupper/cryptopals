2deb614e200e83d83553eea753ee3faaf8d01d8b
Valentin Olchowski <valentin.olchowski@gmail.com>
2019-11-22 17:49:37 +0100

    Script

---
+++ Set1/script.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+  document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+  }, false);
+  document.addEventListener("keydown", function(e) {
+  //document.onkeydown = function(e) {
+    // "I" key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+      disabledEvent(e);
+    }
+    // "J" key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+      disabledEvent(e);
+    }
+    // "S" key + macOS
+    if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+      disabledEvent(e);
+    }
+    // "U" key
+    if (e.ctrlKey && e.keyCode == 85) {
+      disabledEvent(e);
+    }
+    // "F12" key
+    if (event.keyCode == 123) {
+      disabledEvent(e);
+    }
+  }, false);
+  function disabledEvent(e){
+    if (e.stopPropagation){
+      e.stopPropagation();
+    } else if (window.event){
+      window.event.cancelBubble = true;
+    }
+    e.preventDefault();
+    return false;
+  }
+};
+
+window.addEventListener('blur', blurImages);
+window.addEventListener('focus', deBlurImages);
+
+function blurImages() {
+  var images = document.getElementsByTagName("img");
+  for (var i = 0; i < images.length; i++){
+    images[i].style.filter="blur(10px)";
+  }
+}
+
+function deBlurImages() {
+  var images = document.getElementsByTagName("img");
+  for (var i = 0; i < images.length; i++){
+    images[i].style.filter="blur(0px)";
+  }
+}
